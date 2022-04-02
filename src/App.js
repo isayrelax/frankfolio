a910b258938eda4c5d8e1d230a09de6f1b0e6387
isayrelax <frank.piro@gmail.com>
2022-04-01 20:38:04 -0400

    add a tags to 'links', add gradient background to html instead of body

--- src/App.js
+++ src/App.js
@@ -15,26 +15,40 @@ function App() {
         </div>
         
         <div className="links">
-            <div className="link link1">
-                <i className="fab fa-github-square"></i>
-                <p>Github</p>
-            </div>
-            <div className="link link2">
-                <i className="fab fa-linkedin"></i>
-                <p>LinkedIn</p>
-            </div>
-            <div className="link link3">
-                <i className="fas fa-pizza-slice"></i>
-                <p>whodelivers.org</p>
-            </div>
+
+            <a href="https://github.com/isayrelax" target="_blank">
+                <div className="link link1">
+                    <i className="fab fa-github-square"></i>
+                    <p>Github</p>
+                </div>
+            </a>
+
+            <a href="https://linkedin.com/in/frank-piro" target="_blank">
+                <div className="link link2">
+                    <i className="fab fa-linkedin"></i>
+                    <p>LinkedIn</p>
+                </div>
+            </a>
+
+            <a href="https://whodelivers.org" target="_blank">
+                <div className="link link3">
+                    <i className="fas fa-pizza-slice"></i>
+                    <p>whodelivers.org</p>
+                </div>
+            </a>
+
             <div className="link link4">
                 <i className="fas fa-file-alt"></i>
                 <p>resume</p>
             </div>
-            <div className="link link5">
-                <i className="fas fa-envelope"></i>
-                <p>contact</p>
-            </div>
+
+            <a href="mailto:frank_piro@hotmail.com" target="_blank">
+                <div className="link link5">
+                    <i className="fas fa-envelope"></i>
+                    <p>contact</p>
+                </div>
+            </a>
+            
         </div>
     </div>
   );
